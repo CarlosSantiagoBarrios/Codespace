c1f9370dde0f979e61de9581a6018fe1ef03c367
CarlosSantiagoBarrios <carlos.barrios@shopify.com>
2025-01-20 20:34:56 -0500

    .

--- pexelsThumbnail/ETL.js
+++ pexelsThumbnail/ETL.js
@@ -7,6 +7,13 @@
 // Define the API URL and the authorization key
 //change only the '?page' (eq or > 100), and the 'query', as changing the 'per_page' (80) doesn't really make a difference (the # of photos retrieved won't ever be higher than 80)
 
+
+// Load prompt: 
+// --> Perform 100 calls (i.e until const 'page' = 100)
+// --> Each time a call is performed, run the fetchCuratedPhotos(); function as usual, but now, instead of printing out the the results, add them to a                  
+//     finalJsonPhotosLinks array that will eventually contain all the results from run 1 till run 100
+// --> Once having performed all the calls, printout the content of the finalJsonPhotosLinks array
+
 const apiKey = "6EdsN9H0AGKaO5qjDly59dWAJJpXaKZf2pc1AfbuaNeSzGyDGJQxcFqK";
 
 const page = 21;
