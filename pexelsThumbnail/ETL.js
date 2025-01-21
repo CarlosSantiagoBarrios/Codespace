3f789bfa143da499ccc4cdb61f95f8637bf917c6
CarlosSantiagoBarrios <carlos.barrios@shopify.com>
2025-01-20 20:58:14 -0500

    ETL.js enhanced with .json file automatically downloaded with results.
    Next features: 
    
    -Overwrite the already downloaded .json file with the new calls' results without deleting the existing content (rather adding to it)
    
    -Automatically perform calls from page 1 till page 100

--- pexelsThumbnail/ETL.js
+++ pexelsThumbnail/ETL.js
@@ -1,4 +1,6 @@
 // This script makes a GET call on the Pexels API (https://www.https://www.pexels.com/api/documentation)
+// Filters the results to have only the 'id' 'large2x' and 'url' fields
+// Automatically downloads a .json file into the local machine with the results
 
 //Calls
 //curated: https://api.pexels.com/v1/curated?page=10&per_page=80
@@ -8,24 +10,17 @@
 //change only the '?page' (eq or > 100), and the 'query', as changing the 'per_page' (80) doesn't really make a difference (the # of photos retrieved won't ever be higher than 80)
 
 
-// Load prompt: 
-// --> Perform 100 calls (i.e until const 'page' = 100)
-// --> Each time a call is performed, run the fetchCuratedPhotos(); function as usual, but now, instead of printing out the the results, add them to a                  
-//     finalJsonPhotosLinks array that will eventually contain all the results from run 1 till run 100
-// --> Once having performed all the calls, printout the content of the finalJsonPhotosLinks array
-
 const apiKey = "6EdsN9H0AGKaO5qjDly59dWAJJpXaKZf2pc1AfbuaNeSzGyDGJQxcFqK";
 
 const page = 21;
 const perPage = 80;
-const query = 'food';
+const query = 'sea';
 
 const curated = `https://api.pexels.com/v1/curated?page=${page}&per_page=${perPage}`;
-const search = `https://api.pexels.com/v1/search?page=${page}&per_page=${perPage}&query=${encodeURIComponent(query)}`; 
+const search = `https://api.pexels.com/v1/search?page=${page}&per_page=${perPage}&query=${encodeURIComponent(query)}`;
 
-//Api Switch (curated, search)
+// Api Switch (curated, search)
 const apiUrl = `${search}`;
-
 
 // Function to make the GET request
 const fetchCuratedPhotos = async () => {
@@ -45,9 +40,6 @@ const fetchCuratedPhotos = async () => {
         // Parse the JSON response
         const data = await response.json();
 
-        // Print the JSON data
-        // console.log(data);
-
         // Map through the photos and extract the id, large2x URL, and url
         const photosArray = data.photos.map(photo => ({
             id: photo.id,
@@ -57,6 +49,23 @@ const fetchCuratedPhotos = async () => {
 
         // Print the result as a JSON array
         console.log(JSON.stringify(photosArray, null, 2));
+
+        // Create a blob from the photos array
+        const jsonBlob = new Blob([JSON.stringify(photosArray, null, 2)], { type: 'application/json' });
+
+        // Create a download link
+        const url = URL.createObjectURL(jsonBlob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'photos.json'; // Set the default filename
+
+        // Append to body, click the link, and remove it
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        
+        // Revoke the object URL after download
+        URL.revokeObjectURL(url);
 
     } catch (error) {
         console.error('Error fetching data:', error);
