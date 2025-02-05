fd00b88d2018adc12e5ad02d335b36594a04816e
CarlosSantiagoBarrios <carlos.barrios@shopify.com>
2025-02-04 19:16:38 -0500

    .

---
+++ Scripts/Typewriter/typewriterTest.js
@@ -0,0 +1,77 @@
+var quotes = [
+    {
+        "text": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+        "author": "Thomas Edison"
+    },
+    {
+        "text": "You can observe a lot just by watching.",
+        "author": "Yogi Berra"
+    },
+    {
+        "text": "A house divided against itself cannot stand.",
+        "author": "Abraham Lincoln"
+    },
+    {
+        "text": "Difficulties increase the nearer we get to the goal.",
+        "author": "Johann Wolfgang von Goethe"
+    },
+    {
+        "text": "Fate is in your hands and no one else's",
+        "author": "Byron Pulsifer"
+    },
+    {
+        "text": "Be the chief but never the lord.",
+        "author": "Lao Tzu"
+    }
+];
+
+// Function to get a random quote
+function getRandomQuote() {
+    var randomIndex = Math.floor(Math.random() * quotes.length);
+    var quote = quotes[randomIndex];
+    return [
+        quote.text,
+        "\n",
+        "-" + quote.author
+    ];
+}
+
+// Set up text to print, each item in array is a new line
+var aText = getRandomQuote();
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this position
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+
+function typewriter() {
+    sContents = ' ';
+    iRow = Math.max(0, iIndex - iScrollAt);
+    var destination = document.getElementById("typedtext");
+
+    while (iRow < iIndex) {
+        sContents += aText[iRow++] + '<br />';
+    }
+    destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+
+    if (iTextPos++ == iArrLength) {
+        iTextPos = 0;
+        iIndex++;
+
+        if (iIndex != aText.length) {
+            iArrLength = aText[iIndex].length;
+            setTimeout(typewriter, 500);
+        } else {
+            // When the typing is finished, set the border-radius
+            destination.style.borderRadius = "10px";
+            destination.style.border = "1px solid black"; // Optional: Add a border to make the border-radius visible
+        }
+    } else {
+        setTimeout(typewriter, iSpeed);
+    }
+}
+
+typewriter();
